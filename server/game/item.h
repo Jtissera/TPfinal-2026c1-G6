1a67f0e937f334670d215aa35dd644670f8712fd
Maurizio Giannantonio <mgiannantonio@fi.uba.ar>
2026-06-02 17:51:44 -0300

    cambios en items

--- server/game/item.h
+++ server/game/item.h
@@ -6,7 +6,12 @@
 #include <string>
 
 struct Item {
-  uint32_t id = 0;
+  // Sirve para equipar, dropear, vender o consumir este item puntual.
+  uint32_t instanceId = 0;
+
+  // ID estable del catálogo compartido con el cliente.
+  // Debe coincidir con assets/items/items.json.
+  uint32_t catalogId = 0;
   std::string typeName;
   ItemSlot slot = ItemSlot::NONE;
   ItemEffect effect = ItemEffect::NONE;
